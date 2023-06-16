7ff44fe94d7f94f34ceadf57a6deb7212bd3d5fd
Vipasha Sharma <99861144+hnzll@users.noreply.github.com>
2023-06-16 15:52:44 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+
+# In[1]:
+
+
+# Create an ordered list (i.e. numbered) listing 3 commonly used languages for data science in python
+languages = ["Python", "R", "Julia"]
+for i, language in enumerate(languages):
+    print(f"{i + 1}. {language}")
+
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# In[3]:
+
+
+# Create an ordered list listing 3 commonly used libraries in data science in python
+libraries = ["pandas", "numpy", "scikit-learn"]
+for library in libraries:
+    print(library)
+
+
+# # Data Science Tools
+
+# In[8]:
+
+
+# Create a single column table in this cell with the first row containing the header Data Science Tools. The subsequent three rows in the table should indicate three development environment open source tools used in data science.
+# Create the table header
+print("Data Science Tools")
+# Create the table rows
+print("- Jupyter Notebook")
+print("- RStudio")
+print("- Google Colaboratory")
+
+
+# # Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[5]:
+
+
+(3*4)+5
+
+
+# In[6]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+# Convert 200 minutes into hours by diving by 60
+minutes = 200
+hours = minutes / 60
+print(f"{minutes} minutes is {hours} hours.")
+
+
+# # Objectives:
+
+# In[7]:
+
+
+# List popular languages for Data Science in Python
+languages = ["Python", "R", "Julia"]
+for language in languages:
+    print(language)
+
+
+# # Author
+# VIPASHA SHARMA

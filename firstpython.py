720a420530c4604c9c020695e89de9244e32c16e
Vipasha Sharma <99861144+hnzll@users.noreply.github.com>
2023-06-16 15:50:01 +0530

    Create firstpython.py
    Adding a new file

---
+++ firstpython.py
@@ -0,0 +1,31 @@
+# Create an ordered list (i.e. numbered) listing 3 commonly used languages for data science in python
+languages = ["Python", "R", "Julia"]
+for i, language in enumerate(languages):
+    print(f"{i + 1}. {language}")
+    
+    # Create an ordered list listing 3 commonly used libraries in data science in python
+libraries = ["pandas", "numpy", "scikit-learn"]
+for library in libraries:
+    print(library)
+    
+ # Create a single column table in this cell with the first row containing the header Data Science Tools. The subsequent three rows in the table should indicate three development environment open source tools used in data science.
+# Create the table header
+print("Data Science Tools")
+# Create the table rows
+print("- Jupyter Notebook")
+print("- RStudio")
+print("- Google Colaboratory")
+
+#examples of evaluating arithmetic expressions in Python
+(3*4)+5
+
+#This will convert 200 minutes to hours by diving by 60
+# Convert 200 minutes into hours by diving by 60
+minutes = 200
+hours = minutes / 60
+print(f"{minutes} minutes is {hours} hours.")
+
+# List popular languages for Data Science in Python
+languages = ["Python", "R", "Julia"]
+for language in languages:
+    print(language)
